ff21b173a8a40874e2ce65e4f56b8d973213b9fe
Bastián Escribano <bastian.escribano@usach.cl>
2022-07-25 14:42:51 -0400

    [Feature] maps api implemented (needs to configurate the styles)

--- scripts/index.js
+++ scripts/index.js
@@ -9,5 +9,15 @@ particlesJS.load('particles-js', 'particles.json', function() {
   console.log('callback - particles.js config loaded');
 });
 
+var map = L.map('map').setView([-33.4499981, -70.687331]  , 17.4);
+
+
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+}).addTo(map);
+
+L.marker([-33.4499981, -70.687331]).addTo(map)
+    .openPopup();
+
 
 }
