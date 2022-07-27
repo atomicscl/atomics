1eaf496d29186fea83b2576e611ad61090fdc222
Bastián Escribano <bastian.escribano@usach.cl>
2022-07-26 23:39:48 -0400

    [Feature] services

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 window.onload = () =>{
-    setTimeout( () =>{
+
     document.getElementById("loader").style.visibility = "hidden";
-}, 1000)
+
 
 particlesJS.load('particles-js', 'particles.json', function() {
   console.log('callback - particles.js config loaded');
