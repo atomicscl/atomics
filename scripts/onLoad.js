b4dd5837f0cd96cc1b0e1e1139603d32fb5a4d1c
Bastián Escribano <bastian.escribano@usach.cl>
2022-07-27 14:47:42 -0400

    [Feature] responsive view

--- scripts/onLoad.js
+++ scripts/onLoad.js
@@ -11,8 +11,55 @@ window.onload = () =>{
   L.marker([-33.4499981, -70.687331]).addTo(map)
       .openPopup();
 
+
+
   const ctx = document.getElementById('myChart').getContext('2d');
   const myChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+          labels: ['Rojo', 'Azul', 'Amarillo', 'Verde', 'Púrpura', 'Naranja'],
+          datasets: [{
+              label: 'Número de Votos',
+              data: [12, 19, 3, 5, 2, 3],
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(255, 159, 64, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(255, 99, 132, 1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)'
+              ],
+              borderWidth: 1
+          }]
+      },
+      options: {
+
+
+          responsive: true,
+            maintainAspectRatio: false,
+        }
+               
+              }
+          );
+
+
+
+
+
+
+
+
+
+  const ctx2 = document.getElementById('myChart2').getContext('2d');
+  const myChart2 = new Chart(ctx2, {
       type: 'bar',
       data: {
           labels: ['Rojo', 'Azul', 'Amarillo', 'Verde', 'Púrpura', 'Naranja'],
@@ -46,6 +93,14 @@ window.onload = () =>{
           }
       }
   });
+
+
+
+
+
+
+
+
 
   $('.slider').each(function() {
     var $this = $(this);
